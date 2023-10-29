a0eb7ff69cd21e75b6ad923a08640b3c90d7b1cc
thewrecker123 <105166080+thewrecker123@users.noreply.github.com>
2023-10-29 12:32:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+clown_nose= loadImage('https://i.postimg.cc/wjsTjmSN/Pngtree-clown-red-nose-animation-illustration-4568263.png');
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+
+function draw() {
+image(video, 0, 0, 300, 300);
+image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+      noseX = results[0].pose.nose.x-15;
+      noseY = results[0].pose.nose.y-15;
+      console.log("nose x = " + noseX);
+      console.log("nose y = " + noseY)
+        
+    }
+}
